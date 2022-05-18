295f0656eaf9e8cebc83bf639a0d52821affe4d6
RyooChan <fbcks97@naver.com>
2022-05-18 23:39:51 +0900

    ✨ 배치 작업 추가
    - 스프링 스케쥴링

--- src/main/java/com/example/SpringBatchStudy/SpringBatchStudyApplication.java
+++ src/main/java/com/example/SpringBatchStudy/SpringBatchStudyApplication.java
@@ -3,7 +3,9 @@ package com.example.SpringBatchStudy;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling	// 특정 시간마다 배치를 돌려주기 위해서
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchStudyApplication {
